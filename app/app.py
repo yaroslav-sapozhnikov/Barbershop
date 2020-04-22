da7667e1c0097226805fb4cb2c80792b97b497b1
yaroslav_sapozhnikov <yariks417@gmail.com>
2020-04-21 23:09:23 -0700

    'Init'

---
+++ app/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from config import Configuration
+from posts.blueprint import posts
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+
+app.register_blueprint(posts, url_prefix = '/blog')
---
+++ app/config.py
@@ -0,0 +1,3 @@
+class Configuration(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost/tst1'
